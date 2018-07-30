54735790e882500778030d74745ac6ac7a51cd95
pjarmy <32950598+pjarmy@users.noreply.github.com>
2018-07-30 17:13:34 +0800

    0730

--- dataAnalysis/fromZero/08_pandas_frame03.py
+++ dataAnalysis/fromZero/08_pandas_frame03.py
@@ -1,1 +1,10 @@
 # pandas_DataFrame03
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# 频数统计
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# 读数据
+import pandas as pd
+income = pd.read_excel('income.xlsx')
