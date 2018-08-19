e8680730bff87b427eac26b64b4d1b1bcf37926a
pjarmy <32950598+pjarmy@users.noreply.github.com>
2018-08-19 21:44:23 +0800

    201808192144

---
+++ dataAnalysis/DataAnalysis1480/fromZero/24-25Ridge_LASSO Regression.py
@@ -0,0 +1,5 @@
+# LinearRegression.py(线性回归诊断)
+# E:/Documents/GitHub/MyCloud/dataAnalysis/DataAnalysis1480/fromZero/data/
+# F:/GitRespository/MyCloud/dataAnalysis/DataAnalysis1480/fromZero/data/
+
+# http://f.dataguru.cn/thread-598486-1-1.html
