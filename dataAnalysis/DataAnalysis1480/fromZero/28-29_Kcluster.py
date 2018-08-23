ab1989fee03fba9fa8b6a472e65ea4693c7d2ecb
pjarmy <32950598+pjarmy@users.noreply.github.com>
2018-08-23 16:57:11 +0800

    201808231657

--- dataAnalysis/DataAnalysis1480/fromZero/28-29_Kcluster.py
+++ dataAnalysis/DataAnalysis1480/fromZero/28-29_Kcluster.py
@@ -2,6 +2,12 @@
 # E:/Documents/GitHub/MyCloud/dataAnalysis/DataAnalysis1480/fromZero/data/
 # F:/GitRespository/MyCloud/dataAnalysis/DataAnalysis1480/fromZero/data/
 
+#     聚类是一种无监督的挖掘算法，其目的就是将N个样本安装特定的划分为K个簇（K<N），而这个
+# 簇所表现的特征是：簇内样本相似度高（方差小），而簇间的相似度低（方差大）。关于聚类算法有
+# 很多，如 K均值聚类、密度聚类、谱系聚类、最大期望聚类等。
+
+#     本文我们介绍的是 K均值聚类，它是公认的十大挖掘算法之一，其优点是计算速度快、原理易于
+# 理解及聚类效果理想。
 
 
 
