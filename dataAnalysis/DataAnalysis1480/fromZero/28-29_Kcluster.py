6b1a6ef0b144dc05c6551ff91219529b441d1d8c
pjarmy <32950598+pjarmy@users.noreply.github.com>
2018-08-26 23:32:06 +0800

    201808262332

--- dataAnalysis/DataAnalysis1480/fromZero/28-29_Kcluster.py
+++ dataAnalysis/DataAnalysis1480/fromZero/28-29_Kcluster.py
@@ -27,20 +27,28 @@ from sklearn.cluster import KMeans
 # 读取网页中的数据表
 table = []
 for i in range(1,7):
-    table.append(pd.read_html('https://nba.hupu.com/stats/players/pts/%d' %i)[0])
+    table.append(pd.read_html('https://nba.hupu.com/stats/players/pts/%d' %i)[0].iloc[1:,])
 
 # 所有数据纵向合并为数据框
 players = pd.concat(table)
 # 变量重命名
-# columns = ['排名','球员','球队','得分','命中-出手','命中率','命中-三分','三分命中率',
-#         '命中-罚球','罚球命中率','场次','上场时间']
-# players.columns= columns
+columns = ['排名','球员','球队','得分','命中-出手','命中率','命中-三分','三分命中率',
+        '命中-罚球','罚球命中率','场次','上场时间']
+players.columns= columns
 
 # 数据类型转换
 players.得分 = players.得分.astype('float')
 players.命中率 = players.命中率.str[:-1].astype('float')/100
-players.三分命中率 = players.三分命中率.str[:-1].astype('floar')/100
-players.罚球命中率 = players.罚球命中率.str[:-1].satype
+players.三分命中率 = players.三分命中率.str[:-1].astype('float')/100
+players.罚球命中率 = players.罚球命中率.str[:-1].astype('float')/100
+players.场次 = players.场次.astype('int')
+players.上场时间 = players.上场时间.astype('float')
+
+# 删除行标签为 0 的记录
+players.drop(0, inplace=True)
+players.head()
+
+
 
 
 
